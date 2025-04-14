b44f31d7054b148b8fe0078fc1713aacaaeb8128
rua093 <playinggamer03@gmail.com>
2025-04-14 12:13:05 +0700

    First commit

---
+++ scope.js
@@ -0,0 +1,13 @@
+const age = 18
+{
+    {
+        {
+            const age = 14
+            {
+                const age = 12
+                console.log(age)
+            }
+            console.log(age)
+        }
+    }
+}
